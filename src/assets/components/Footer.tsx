3eb53f64cf72a4a1d5e735ec1e79d9cf27ad0f38
Pande <popovski.pande@gmail.com>
2023-08-15 15:51:49 +0200

    set up footer

--- src/assets/components/Footer.tsx
+++ src/assets/components/Footer.tsx
@@ -1,7 +1,9 @@
 export default function Footer() {
   return(
     <footer className="footer">
-      <p className='text'>&copy; Copyright 2023, Pande Popovski</p>
+      <p 
+        className='w-5/6 mx-6 mt-6 text-lg text-center py-4 text-textSecondaryLight dark:text-textSecondaryDark transition-colors duration-1000 ease-in-out'>
+          &copy; Copyright 2023, Pande Popovski</p>
     </footer>
   )
 };
